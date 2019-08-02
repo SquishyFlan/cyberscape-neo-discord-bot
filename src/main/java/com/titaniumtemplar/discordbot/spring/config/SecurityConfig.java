3a10df39a92e0dc756c76640b7c0c930cfa35a9d
Zack Young <zyoung@jive.com>
2019-08-02 12:08:57 -0600

    Fix public profile security from traliing slash bug

--- src/main/java/com/titaniumtemplar/discordbot/spring/config/SecurityConfig.java
+++ src/main/java/com/titaniumtemplar/discordbot/spring/config/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.csrf().disable()
 			.authorizeRequests()
 			.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-			.antMatchers("/profile/*").permitAll()
+			.mvcMatchers("/profile/**").permitAll()
 			.anyRequest().authenticated()
 			.and()
 			.oauth2Login()
